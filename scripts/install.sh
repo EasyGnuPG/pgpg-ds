88b6003fe03071bcf8c09f9ece2c94d7193553c7
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-05-25 17:05:44 +0530

    Update for pgpg
    closes EasyGnuPG/pgpg#19

---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+cd /host/pgpg/
+pip3 install --editable .
--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
-APP=egpg-buster
-IMAGE=egpg-buster
-CONTAINER=egpg-buster
+APP=pgpg-buster
+IMAGE=pgpg-buster
+CONTAINER=pgpg-buster
 PORTS="4000:4000"
